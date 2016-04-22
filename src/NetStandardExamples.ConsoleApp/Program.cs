4a0b2f75951bc450f80cea479a7a28d85793e743
Jeff Papp <jeff@jeffpapp.com>
2016-04-22 15:36:21 -0600

    Build issues

--- src/NetStandardExamples.ConsoleApp/Program.cs
+++ src/NetStandardExamples.ConsoleApp/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using NetstandardExamples.Shared;
+using System.Linq;
 
-namespace ConsoleApplication
+namespace NetStandardExamples.ConsoleApp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var services = new ServiceCollection();
+            
+            services.AddEntityFrameworkSqlServer()
+                .AddDbContext<AppDbContext>();
+
+            
+
+            var serviceProvider  = services.BuildServiceProvider();
+            
+            
+            var dbContext = serviceProvider.GetService<AppDbContext>();
+            
+            dbContext.Database.EnsureCreated();
+            
+            var people = dbContext.People.ToList();
+            
+            Console.WriteLine($"Hello World!  {people.Count} people in the db");
         }
     }
 }
